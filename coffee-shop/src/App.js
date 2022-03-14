c82e6545ecaab2cf916e437298cbef52d873f4db
Stephen Kaiser <stephen.kaiser@onepeloton.com>
2022-03-14 08:40:04 -0700

    Initial HTML setup and image assets

--- coffee-shop/src/App.js
+++ coffee-shop/src/App.js
@@ -1,27 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Apple from './items/apple.svg';
+import Coffee from './items/coffee.svg';
+import Cookie from './items/cookie.svg';
+import Tea from './items/tea.svg';
+import Wine from './items/wine.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Coffee Shop</h1>
       </header>
+      <div>
+        <a href="#todo">
+          <img src={Apple} alt="Apple" />
+          <div>Apple</div>
+        </a>
+        <a href="#todo">
+          <img src={Coffee} alt="Coffee" />
+          <div>Coffee</div>
+        </a>
+        <a href="#todo">
+          <img src={Cookie} alt="Cookie" />
+          <div>Cookie</div>
+        </a>
+        <a href="#todo">
+          <img src={Tea} alt="Tea" />
+          <div>Tea</div>
+        </a>
+        <a href="#todo">
+          <img src={Wine} alt="Wine" />
+          <div>Wine</div>
+        </a>
+      </div>
     </div>
   );
 }
