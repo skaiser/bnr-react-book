9f970ed0f58ac1ef8664559e1dc2537f5a2d23dc
Stephen Kaiser <stephen.kaiser@onepeloton.com>
2022-03-16 09:13:08 -0700

    11-fetch-items from backend

--- coffee-shop/src/App.js
+++ coffee-shop/src/App.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import Cart from './components/Cart';
 import Details from './components/Details';
@@ -7,30 +8,42 @@ import Header from './components/Header';
 import Home from './components/Home';
 import PageNotFound from './components/PageNotFound';
 import { CartTypes, useCartReducer } from './reducers/cartReducer';
-import { items } from './items';
 
 function App() {
   const [cart, dispatchCart] = useCartReducer();
+  const [items, setItems] = useState([]);
   const addToCart = useCallback(
-    (itemId) => dispatchCart({ type: CartTypes.ADD, itemId }),
+    (itemId) => {
+      dispatchCart({ type: CartTypes.ADD, itemId });
+    },
     [dispatchCart],
   );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('/api/items');
+        setItems(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <Router>
       <Header cart={cart} />
-      <Routes>
-        <Route
-          path="/cart"
-          element={<Cart cart={cart} items={items} dispatch={dispatchCart} />}
-        />
-        <Route
-          path="/details/:id"
-          element={<Details addToCart={addToCart} items={items} />}
-        />
-        <Route path="/" element={<Home items={items} />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      {items.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <Routes>
+          <Route path="/cart" element={<Cart cart={cart} items={items} dispatch={dispatchCart} />} />
+          <Route path="/details/:id" element={<Details addToCart={addToCart} items={items} />} />
+          <Route path="/" element={<Home items={items} />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      )}
     </Router>
   );
 }
