4088eb9716304b6145a6517b8e5630766c0d2b4e
Stephen Kaiser <stephen.kaiser@onepeloton.com>
2022-03-17 13:41:36 -0700

    17-end-to-end-testing: Add cypress and initial e2e tests

---
+++ coffee-shop/cypress/integration/orders.spec.js
@@ -0,0 +1,33 @@
+describe('Orders', () => {
+  before(() => {
+    cy.request('DELETE', '/api/orders');
+  });
+
+  it('User can build a cart and place an order', () => {
+    cy.visit('/');
+    cy.findByRole('link', { name: /Apple/ }).click();
+    cy.findByText(/Price:/).should('exist');
+    cy.findByRole('button', { name: 'Add to Cart' }).click();
+    cy.findByTestId('cart-quantity').should('contain', '1');
+    cy.findByRole('button', { name: 'Add to Cart' }).click();
+    cy.findByTestId('cart-quantity').should('contain', '2');
+    cy.findByRole('link', { name: /Cart/i }).click();
+    cy.findByLabelText(/Name/).should('exist');
+    cy.findByRole('button', { name: 'Place Order' }).should('be.disabled');
+    cy.findByLabelText(/Name/).type('Big Nerd Ranch');
+    cy.findByLabelText(/Zip Code/).type('30307');
+    cy.findByRole('button', { name: 'Place Order' }).should('be.enabled');
+    cy.findByRole('button', { name: 'Place Order' }).click();
+    cy.findByText(/Thanks for your order/).should('exist');
+    cy.findByRole('button', { name: /Return home/i }).click();
+    cy.findAllByTestId('thumbnail-component');
+  });
+
+  it('Can View and Delete Orders', () => {
+    cy.visit('/');
+    cy.login();
+    cy.findByRole('link', { name: 'Orders' }).click();
+    cy.findByRole('button', { name: 'Delete Order' }).click();
+    cy.findByText(/No Orders/).should('exist');
+  });
+});
--- coffee-shop/src/components/Login.test.js
+++ coffee-shop/src/components/Login.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
